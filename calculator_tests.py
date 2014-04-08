77a6ecaca07c43789d008750301e56b877e3f010
Kevin Glidewell <kpglide@gmail.com>
2014-04-08 18:09:56 -0400

    working version of discount calculator

--- calculator_tests.py
+++ calculator_tests.py
@@ -8,11 +8,11 @@ class Discount_Calculator_TDD(unittest.TestCase):
 		self.calc = Discount_Calculator()
 
 	def test_calculate_discount_method_percent_input(self):
-		result = self.calc.calculate_discount(100, 10, 'percent')
-		self.assertEqual(10, result)
+		result = self.calc.calculate_discount(250, 10, 'percent')
+		self.assertEqual(25, result)
 
 	def test_calculate_discount_method_whole_num_input(self):
-		result = self.calc.calculate_discount(100, 20, 'whole_num')
+		result = self.calc.calculate_discount(250, 20, 'absolute')
 		self.assertEqual(20, result)
 
 	def test_calculator_returns_error_message_if_both_args_not_numbers(self):
@@ -22,5 +22,21 @@ class Discount_Calculator_TDD(unittest.TestCase):
 		self.assertRaises(ValueError, self.calc.calculate_discount, 'two', 3, 'percent')
 
 	def test_calculator_returns_error_message_if_y_arg_not_number(self):
-			self.assertRaises(ValueError, self.calc.calculate_discount, 2, 'three', 'percent')
+		self.assertRaises(ValueError, self.calc.calculate_discount, 2, 'three', 'percent')
 
+	def test_handles_floats_percent(self):
+		result = self.calc.calculate_discount(250.0, 10.0, 'percent')
+		self.assertEqual(25, result)
+
+	def test_handles_floats_absolute(self):
+		result = self.calc.calculate_discount(250.0, 10.0, 'absolute')
+		self.assertEqual(10, result)	
+	
+	def test_invalid_discount_type(self):
+		self.assertRaises(ValueError, self.calc.calculate_discount, 250, 10, 'random')
+
+	def test_discount_greater_than_total(self):
+		self.assertRaises(ValueError, self.calc.calculate_discount, 200, 201, 'percent')
+
+	def test_discount_percent_over_one_hundred(self):
+		self.assertRaises(ValueError, self.calc.calculate_discount, 200, 101, 'percent')		
--- discount_calculator.py
+++ discount_calculator.py
@@ -1,12 +1,24 @@
 class Discount_Calculator(object):
 
-	def calculate_discount(self, cart_amount, discount_amount, typ):
+	def calculate_discount(self, cart_amount, discount_amount, discount_type):
+		
 		if type(cart_amount) != int and type(cart_amount) != float:
 			raise ValueError
 		elif type(discount_amount) != int and type(discount_amount) != float:
 			raise ValueError
-		elif typ == 'percent':
-			discount = (discount_amount / 100.0) * cart_amount
-		else:
-			discount = discount_amount
+		
+		elif discount_type == 'percent':
+			if discount_amount > 100:
+				raise ValueError('Percentage must be 100 or less')
+			else: 
+				discount = (discount_amount / 100.0) * cart_amount
+		
+		elif discount_type == 'absolute':
+			if discount_amount > cart_amount:
+				raise ValueError('Discount cannot exceed cart total')
+			else:
+				discount = discount_amount
+		
+		else: raise ValueError('Invalid discount type')
+		
 		return discount
